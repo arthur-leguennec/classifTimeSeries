479c443d0b901a7dc9dae44cfa0e65cfc834b162
arthur56 <arthur.leguennec@gmail.com>
2016-02-23 14:12:32 +0100

    Change the directory of UCR data

--- cmdLine.lua
+++ cmdLine.lua
@@ -10,14 +10,14 @@ cmd:option('-pathData', '', 'path to file with data')
 cmd:option('-fileTrain', '', 'filename for training')
 cmd:option('-fileTest', '', 'filename for testing')
 cmd:option('-noModeCuda', false, 'activate the mode cuda')
-cmd:option('-lr', 0.02, 'learning rate')
-cmd:option('-iter', 5, 'max iteration for training')
+cmd:option('-lr', 0.01, 'learning rate')
+cmd:option('-iter', 15, 'max iteration for training')
 params = cmd:parse(arg)
 
 if params.pathData == '' then
     if params.fileTrain == '' or params.fileTest == '' then
-        fileTrain = '/home/arthur/Downloads/UCR_TS_Archive_2015/ECG5000/ECG5000_TRAIN'
-        fileTest = '/home/arthur/Downloads/UCR_TS_Archive_2015/ECG5000/ECG5000_TEST'
+        fileTrain = '../UCR_TS_Archive_2015/ECG5000/ECG5000_TRAIN'
+        fileTest = '../UCR_TS_Archive_2015/ECG5000/ECG5000_TEST'
     else
         fileTrain = params.fileTrain
         fileTest = params.fileTest
