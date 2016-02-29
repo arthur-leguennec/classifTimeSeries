2dac48fa2df08bca73f9ed8f9ad947816286e5db
arthur56 <arthur.leguennec@gmail.com>
2016-02-29 13:47:05 +0100

    Add file script.sh for testing several dataset

---
+++ start.sh
@@ -0,0 +1,263 @@
+# Script bash for save the results
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG200/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECG5000/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting2/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Lighting7/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/wafer/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Wine/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Worms/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/MedicalImages/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/ECGFiveDays/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Earthquakes/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/OliveOil/ -lr 0.1 -iter 500
+
+###########################################################################
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.01 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.02 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.05 -iter 50
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.1 -iter 50
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.01 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.02 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.05 -iter 100
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.1 -iter 100
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.01 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.02 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.05 -iter 200
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.1 -iter 200
+
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.01 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.02 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.05 -iter 500
+th main.lua -pathData ../UCR_TS_Archive_2015/Meat/ -lr 0.1 -iter 500
