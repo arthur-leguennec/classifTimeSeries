e44acbf1dbf95798e3432e836a33161ff0672c48
arthur56 <arthur.leguennec@gmail.com>
2016-02-29 11:15:53 +0100

    Add a variable id for each neural network

--- model.lua
+++ model.lua
@@ -22,94 +22,70 @@ function neuralNetwork0()
     net:add(nn.Linear(500, nb_class))
     net:add(nn.LogSoftMax())
 
+    id = '200-200-500 (fully connected)'
+
     print('Neural Network 0\n' .. net:__tostring())       -- print the model
 end
 
 
 function neuralNetwork1()
-    net:add(nn.TemporalConvolution(1, 1, 5))
+    local firstInputConv = 5
+    local firstOutputConv = 1
+    local firstMaxPool = 5
+    local secondInputConv = 5
+    local secondOutputConv = 1
+
+    net:add(nn.TemporalConvolution(1, firstOutputConv, firstInputConv))
     net:add(nn.ReLU())
-    net:add(nn.TemporalMaxPooling(5))
-    net:add(nn.TemporalConvolution(1, 1, 5))
+    net:add(nn.TemporalMaxPooling(firstMaxPool))
+    net:add(nn.TemporalConvolution(1, secondOutputConv, secondInputConv))
     net:add(nn.ReLU())
-    net:add(nn.View(sizeData/5 - 5))
-    net:add(nn.Linear(sizeData/5 - 5, 200))        -- fully connected layer (matrix multiplication between inputs and weights)
+    local num = (math.floor((sizeData - firstInputConv + 1) / firstMaxPool) * firstOutputConv - secondInputConv + 1) * secondOutputConv
+    net:add(nn.View(num))
+    net:add(nn.Linear(num, 200))        -- fully connected layer (matrix multiplication between inputs and weights)
     net:add(nn.ReLU())
     net:add(nn.Linear(200, 500))        -- fully connected layer (matrix multiplication between inputs and weights)
     net:add(nn.ReLU())
     net:add(nn.Linear(500, nb_class))
     net:add(nn.LogSoftMax())
+
+    id = 'conv(' .. firstInputConv .. ',' .. firstOutputConv .. ')-maxPool(' .. firstMaxPool ..
+        ')-conv(' .. secondInputConv .. ',' .. secondOutputConv .. ')-' .. num .. '-200-500'
 
     print('Neural Network 1\n' .. net:__tostring())       -- print the model
 end
 
 
-function neuralNetwork2()
-    net:add(nn.TemporalConvolution(1, 1, 10))
-    net:add(nn.ReLU())
-    net:add(nn.TemporalMaxPooling(10))
-    net:add(nn.TemporalConvolution(1, 1, 5))
-    net:add(nn.ReLU())
-    net:add(nn.View(sizeData/10 - 5))
-    net:add(nn.Linear(sizeData/10 - 5, 200))        -- fully connected layer (matrix multiplication between inputs and weights)
-    net:add(nn.ReLU())
-    net:add(nn.Linear(200, 500))        -- fully connected layer (matrix multiplication between inputs and weights)
-    net:add(nn.ReLU())
-    net:add(nn.Linear(500, nb_class))
-    net:add(nn.LogSoftMax())
-
-    print('Neural Network 2\n' .. net:__tostring())       -- print the model
-end
-
-
-function neuralNetwork3()
-    net:add(nn.TemporalConvolution(1, 1, 10))
-    net:add(nn.ReLU())
-    net:add(nn.Dropout())
-    net:add(nn.TemporalMaxPooling(10))
-    net:add(nn.TemporalConvolution(1, 1, 10))
-    net:add(nn.ReLU())
-    net:add(nn.Dropout())
-    net:add(nn.View(sizeData/10 - 10))
-    net:add(nn.Linear(sizeData/10 - 10, 200))        -- fully connected layer (matrix multiplication between inputs and weights)
-    net:add(nn.ReLU())
-    net:add(nn.Dropout())
-    net:add(nn.Linear(200, 500))        -- fully connected layer (matrix multiplication between inputs and weights)
-    net:add(nn.ReLU())
-    net:add(nn.Dropout())
-    net:add(nn.Linear(500, nb_class))
-    net:add(nn.LogSoftMax())
-
-    print('Neural Network 3\n' .. net:__tostring())       -- print the model
-end
-
-
 function neuralNetworkLenet()
-    firstInputConv = 5
-    secondInputConv = 10
-    firstOutputConv = 1
-    secondOutputConv = 3
-    firstMaxPool = 2
-    secondMaxPool = 2
+    local firstInputConv = 5
+    local secondInputConv = 3
+    local firstOutputConv = 1
+    local secondOutputConv = 3
+    local firstMaxPool = 2
+    local secondMaxPool = 2
 
     net:add(nn.TemporalConvolution(1, firstOutputConv, firstInputConv))
     net:add(nn.ReLU())
-    -- net:add(nn.Dropout())
+    -- net:add(nn.Dropout(0.5))
     net:add(nn.TemporalMaxPooling(firstMaxPool))
     net:add(nn.TemporalConvolution(1, secondOutputConv, secondInputConv))
     net:add(nn.ReLU())
-    -- net:add(nn.Dropout())
+    -- net:add(nn.Dropout(0.5))
     net:add(nn.TemporalMaxPooling(secondMaxPool))
     local num = (  math.floor((math.floor((sizeData-firstInputConv+1)/firstMaxPool)*firstOutputConv - secondInputConv + 1)/secondMaxPool)*secondOutputConv  )
     net:add(nn.View(num))
     net:add(nn.Linear(num, 120))
     net:add(nn.ReLU())
-    -- net:add(nn.Dropout())
+    -- net:add(nn.Dropout(0.5))
     net:add(nn.Linear(120, 84))
     net:add(nn.ReLU())
     -- net:add(nn.Dropout())
     net:add(nn.Linear(84, nb_class))
-    net:add(nn.LogSoftMax())
+    -- net:add(nn.LogSoftMax())
+
+    id = 'conv(' .. firstInputConv .. ',' .. firstOutputConv .. ')-maxPool(' .. firstMaxPool ..
+        ')-conv(' .. secondInputConv .. ',' .. secondOutputConv .. ')-maxPool(' .. secondMaxPool ..
+        ')-' .. num .. '-120-84'
 
     print('Lenet\n' .. net:__tostring())       -- print the model
 end
